53c20229443b1a33296aebd6697b423bb976230e
doomkinn@yandex.ru <doomkinn@yandex.ru>
2021-11-18 17:06:23 +0300

    Добавьте файлы проекта.

---
+++ OOP_lesson6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
