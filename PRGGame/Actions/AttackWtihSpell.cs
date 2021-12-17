02dbb36ff5cdebcdadc3bea3d185ea5eb01c0417
doomkinn-geek <doomkinn@yandex.ru>
2021-12-17 22:34:45 +0300

    added more actions

---
+++ PRGGame/Actions/AttackWtihSpell.cs
@@ -0,0 +1,25 @@
+﻿using PRGGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRGGame.Actions
+{    
+    public class AttackWtihSpell : BaseAction, IAction
+    {
+        private readonly int _damage;
+        private readonly int _mana;
+
+        public AttackWtihSpell(Item spellInUse, int magicDamage, int manaCost) :
+            base(spellInUse)
+        {
+            _damage = magicDamage;
+            _mana = manaCost;
+        }
+
+        public void Execute(Creature actor, Creature victim)
+        {
+            int damage = _damage;            
+        }
+    }
+}
